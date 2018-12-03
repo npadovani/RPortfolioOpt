af2f2e19efaea4f9bcd170a76f30dc561bdddf32
Nicola Padovani <n.t.g.padovani@gmail.com>
2018-12-03 18:29:37 +0000

    second change

--- Drawdown at risk efficient portfolios.R
+++ Drawdown at risk efficient portfolios.R
@@ -5,8 +5,8 @@ library(fPortfolio)
 data("StockIndex")
 data("edhec")
 
-popt <- PMaxDD(PriceData = StockIndex,MaxDD = 0.10,softBudget = TRUE)
-popt
+popt_1 <- PMaxDD(PriceData = StockIndex,MaxDD = 0.10,softBudget = TRUE)
+popt_1
 popt_ed <- PMaxDD(PriceData = edhec,MaxDD = 0.10,softBudget = FALSE)
 
 p_DAR <- PCDaR(StockIndex,alpha = 0.90,bound = 0.07,softBudget = FALSE)

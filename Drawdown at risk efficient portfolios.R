efced1b29c5286fdc2e64226ce229627cd110ee4
Nicola Padovani <n.t.g.padovani@gmail.com>
2018-12-03 16:58:15 +0000

    Changes to name of MaxDD results weight variable to popt_1

---
+++ Drawdown at risk efficient portfolios.R
@@ -0,0 +1,12 @@
+library(PortfolioAnalytics)
+library(FRAPO)
+library(tseries)
+library(fPortfolio)
+data("StockIndex")
+data("edhec")
+
+popt <- PMaxDD(PriceData = StockIndex,MaxDD = 0.10,softBudget = TRUE)
+popt
+popt_ed <- PMaxDD(PriceData = edhec,MaxDD = 0.10,softBudget = FALSE)
+
+p_DAR <- PCDaR(StockIndex,alpha = 0.90,bound = 0.07,softBudget = FALSE)

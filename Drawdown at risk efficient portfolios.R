ff33ea533ed724aa7024b42053196e7c976c9c63
Nicola Padovani <n.t.g.padovani@gmail.com>
2018-12-03 19:26:00 +0000

    changed name of MaxDD variable to popt_2

--- Drawdown at risk efficient portfolios.R
+++ Drawdown at risk efficient portfolios.R
@@ -5,8 +5,8 @@ library(fPortfolio)
 data("StockIndex")
 data("edhec")
 
-popt_1 <- PMaxDD(PriceData = StockIndex,MaxDD = 0.10,softBudget = TRUE)
-popt_1
+popt_2 <- PMaxDD(PriceData = StockIndex,MaxDD = 0.10,softBudget = TRUE)
+popt_2
 popt_ed <- PMaxDD(PriceData = edhec,MaxDD = 0.10,softBudget = FALSE)
 
 p_DAR <- PCDaR(StockIndex,alpha = 0.90,bound = 0.07,softBudget = FALSE)
